11488e0fb50e7b1aa9cbd7ed386c9ab9e3ac2221
yungDoom <cagataybugrafan@gmail.com>
2025-04-05 16:48:43 +0300

    change assets directory.

--- asset/index.php
+++ asset/index.php
@@ -1,11 +1,15 @@
 <?php
 ob_start();
 $id = (int)$_GET["id"];
-$file = $id;
+$folderPath = "assets/";
+$file = $folderPath . $id;
+
 if (!file_exists($file)) {
     $file = "https://www.roblox.com/asset/?id=" . $id;
     header("location:" . $file);
+    exit();
 }
+
 readfile($file);
 header("content-type:text/plain");
 ?>
