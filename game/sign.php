8c738e5ed19551f8504a66774ec9c6b05ec4b5b6
yungDoom <cagataybugrafan@gmail.com>
2025-04-06 13:48:47 +0300

    changes

--- game/sign.php
+++
@@ -1,8 +0,0 @@
-<?php   
-    function get_signature($script)
-    {
-        $signature = "";
-        openssl_sign($script, $signature, file_get_contents("..\signing\privatekey.pem"), OPENSSL_ALGO_SHA1);
-        return base64_encode($signature);
-    }
-?>
