9143acc4692aa8629862655ce2349af6f2b64193
yungDoom <cagataybugrafan@gmail.com>
2025-04-09 13:29:14 +0300

    nn

--- marketplace/productinfo/index.php
+++
@@ -1,6 +0,0 @@
-{"AssetId":1,"ProductId":1,"Name":"<?php if ($_GET["assetId"] == "1818") {
-	echo "Rocknet";
-	} else {
-		echo "Non-Place Product";
-	}
- ?>","Description":"Rocknet","AssetTypeId":19,"Creator":{"Id":1,"Name":"yungDoom","CreatorType":"User","CreatorTargetId":1},"IconImageAssetId":0,"Created":"2012-09-28T01:09:47.077Z","Updated":"2017-01-03T00:25:45.8813192Z","PriceInRobux":null,"PriceInTickets":null,"Sales":0,"IsNew":false,"IsForSale":true,"IsPublicDomain":false,"IsLimited":false,"IsLimitedUnique":false,"Remaining":null,"MinimumMembershipLevel":0,"ContentRatingTypeId":0}
