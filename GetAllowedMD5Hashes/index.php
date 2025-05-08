135570ae95ceb1ebea6957d6e1269843ec2ff5c1
yungDoom <cagataybugrafan@gmail.com>
2025-05-08 15:54:18 +0300

    yes

--- GetAllowedMD5Hashes/index.php
+++ GetAllowedMD5Hashes/index.php
@@ -2,4 +2,4 @@
 // If your client is giving errors, launch genmd5.bat inside Trunk2016 and put that MD5 hash to here.
 ?>
 
-{"data":["c8768de038c41af2e81f5f2eb9a14a12"]}
+{"data":["153c6e1343da288e9f3c58504dd46d21"]}
