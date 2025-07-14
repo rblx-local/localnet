b3aedfd01f1dcb38aec58960d5c3558548691818
yungDoom <cagataybugrafan@gmail.com>
2025-07-14 17:43:53 +0300

    extra hash for meeee

--- GetAllowedMD5Hashes/index.php
+++ GetAllowedMD5Hashes/index.php
@@ -2,4 +2,4 @@
 // If your client is giving errors, launch genmd5.bat inside Trunk2016 and put that MD5 hash to here.
 ?>
 
-{"data":["5bb57db8050a120be4f120bcd9dd9cbd"]}
+{"data":["5bb57db8050a120be4f120bcd9dd9cbd","c50e7b2a078f950b165fcaf182a3a82e"]}
